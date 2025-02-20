f167c5f7bbaef215a287e5d1014450c197baff38
zhaoyj <421321729@qq.com>
2025-02-20 16:46:54 +0800

    test

--- src/main/java/com/zhao/springboot/shop/ShopApplication.java
+++ src/main/java/com/zhao/springboot/shop/ShopApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ShopApplication {
 
     public static void main(String[] args) {
-        //这里是主版本
+        //这里是branch1
         SpringApplication.run(ShopApplication.class, args);
     }
 
